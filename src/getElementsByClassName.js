8092f5460cd3740373377105fc65621cbcf770af
Plora Tran <ploratran@gmail.com>
2019-05-27 14:37:40 -0700

    precourse submit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //your code goes here
+  var res = [];
+  var findElementsWithClassName = function(element){
+    //JS Element.classList is a read-only property that returns a live DOMTokenList collection of the class attributes of the element.
+    //contains( String ) - Checks if the specified class value exists in the element's class attribute.
+    if(element.classList && element.classList.contains(className)){
+      res.push(element);
+    }
+    //Node.childNodes read-only property returns a live NodeList of child nodes of the given element where the first child node is assigned index 0.
+    //use recursion - check if classname has childnodes in the DOM then add to the res
+    if(element.childNodes){
+      for(var i = 0; i < element.childNodes.length; ++i){
+        findElementsWithClassName(element.childNodes[i]);
+      }
+    }
+   }
+   //Use recursion to the body of the DOM
+   //The Document.body property represents the <body> or <frameset> node of the current document, or null if no such element exists.
+   findElementsWithClassName(document.body);
+   return res;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,56 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  var str = '';
+
+  //when obj is a num or boolean
+  if (typeof obj === 'number'|| typeof obj === 'boolean'){
+    //simply add '';
+    str += obj;
+  }
+
+  // expected null to equal 'null'
+  if (obj === null){
+    return "null";
+  }
+
+  //when obj is string: expected '' to equal '"Hello world"' with " Ha "
+  if (typeof obj === 'string'){
+    obj = '"' + obj + '"';
+    str += obj;
+  }
+
+  //when its an array of length 0 expected '{}' to equal '[]'
+  if (Array.isArray(obj) && obj.length === 0){
+    return str+= '[]';
+  }
+
+  //an array with more than 1 element
+  if (Array.isArray(obj) && obj.length > 0){
+    var res = []
+
+    obj.forEach(element => {
+      //recursion to stringify each element
+      res.push(stringifyJSON(element));
+    });
+    // expected '' to equal '[8]'
+    return str += '[' + res.join(',') + ']';
+  }
+
+  //when it's an object
+  if (typeof obj === 'object'){
+
+    for (var key in obj){
+      //if value is undefined or a function
+      if(obj[key] !== undefined && typeof obj[key] !== 'function'){
+        //recursion to stringify key and value
+        str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    return '{' + str.slice(0,str.length-1) + '}';
+  }
+
+  return str;
+
 };
